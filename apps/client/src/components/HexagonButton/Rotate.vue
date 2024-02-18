52671192235d4a4791f65a2ddc34c21806a78aa9
DanielCherman <daniel10050181@gmail.com>
2024-02-18 19:22:40 +0100

    add rotate button

--- apps/client/src/components/HexagonButton/Rotate.vue
+++ apps/client/src/components/HexagonButton/Rotate.vue
@@ -1,29 +1,76 @@
 <template>
-  <HexagonButtonBase>
+  <button class="icon-fill-primary-default hover:icon-fill-white group relative size-[58px] [&_path]:transition-all">
+    <div class="relative z-[11]">
+      <svg
+        class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-all duration-500 group-hover:rotate-180 group-hover:scale-[1.2]"
+        xmlns="http://www.w3.org/2000/svg"
+        width="38"
+        height="38"
+        viewBox="0 0 48 49"
+        fill="none"
+      >
+        <g clip-path="url(#clip0_61_1953)">
+          <path
+            d="M37.999 16.6448L29.999 24.6448H35.999C35.999 31.2748 30.629 36.6448 23.999 36.6448C21.969 36.6448 20.069 36.1348 18.389 35.2548L15.469 38.1748C17.949 39.7248 20.859 40.6448 23.999 40.6448C32.839 40.6448 39.999 33.4848 39.999 24.6448H45.999L37.999 16.6448ZM11.999 24.6448C11.999 18.0148 17.369 12.6448 23.999 12.6448C26.029 12.6448 27.929 13.1548 29.609 14.0348L32.529 11.1148C30.049 9.56478 27.139 8.64478 23.999 8.64478C15.159 8.64478 7.99902 15.8048 7.99902 24.6448H1.99902L9.99902 32.6448L17.999 24.6448H11.999Z"
+            fill="#030303"
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_61_1953">
+            <rect
+              width="48"
+              height="48"
+              fill="white"
+              transform="translate(-0.000976562 0.644775)"
+            />
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
     <svg
-      class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-all duration-500 group-hover:rotate-180 group-hover:scale-[1.2]"
+      class="absolute left-0 top-0 z-[9] size-full"
       xmlns="http://www.w3.org/2000/svg"
-      width="38"
-      height="38"
-      viewBox="0 0 48 49"
+      width="88"
+      height="83"
+      viewBox="0 0 88 83"
       fill="none"
     >
-      <g clip-path="url(#clip0_61_1953)">
+      <path
+        d="M2.69245 51.6448C-0.880208 45.4568 -0.880208 37.8328 2.69245 31.6448L14.686 10.8714C18.2586 4.68336 24.8612 0.871384 32.0065 0.871384H55.9935C63.1388 0.871384 69.7414 4.68336 73.314 10.8714L85.3076 31.6448C88.8802 37.8328 88.8802 45.4568 85.3075 51.6448L73.314 72.4182C69.7414 78.6062 63.1388 82.4182 55.9935 82.4182H32.0065C24.8612 82.4182 18.2586 78.6062 14.686 72.4182L2.69245 51.6448Z"
+        fill="var(--iconColor)"
+      />
+    </svg>
+    <svg
+      class="absolute left-1/2 top-1/2 z-[10] -translate-x-1/2 -translate-y-1/2 mix-blend-screen"
+      xmlns="http://www.w3.org/2000/svg"
+      width="76"
+      height="71"
+      viewBox="0 0 76 71"
+      fill="none"
+    >
+      <g style="mix-blend-mode:screen">
         <path
-          d="M37.999 16.6448L29.999 24.6448H35.999C35.999 31.2748 30.629 36.6448 23.999 36.6448C21.969 36.6448 20.069 36.1348 18.389 35.2548L15.469 38.1748C17.949 39.7248 20.859 40.6448 23.999 40.6448C32.839 40.6448 39.999 33.4848 39.999 24.6448H45.999L37.999 16.6448ZM11.999 24.6448C11.999 18.0148 17.369 12.6448 23.999 12.6448C26.029 12.6448 27.929 13.1548 29.609 14.0348L32.529 11.1148C30.049 9.56478 27.139 8.64478 23.999 8.64478C15.159 8.64478 7.99902 15.8048 7.99902 24.6448H1.99902L9.99902 32.6448L17.999 24.6448H11.999Z"
-          fill="#030303"
+          d="M2.96198 45.6448C-0.610678 39.4568 -0.610676 31.8328 2.96198 25.6448L11.8207 10.301C15.3934 4.11299 21.9959 0.301018 29.1412 0.301018H46.8587C54.004 0.301018 60.6066 4.113 64.1792 10.301L73.038 25.6448C76.6106 31.8328 76.6106 39.4568 73.038 45.6448L64.1792 60.9886C60.6066 67.1766 54.004 70.9886 46.8587 70.9886H29.1412C21.9959 70.9886 15.3934 67.1766 11.8207 60.9886L2.96198 45.6448Z"
+          fill="url(#paint0_linear_61_1952)"
         />
       </g>
       <defs>
-        <clipPath id="clip0_61_1953">
-          <rect
-            width="48"
-            height="48"
-            fill="white"
-            transform="translate(-0.000976562 0.644775)"
+        <linearGradient
+          id="paint0_linear_61_1952"
+          x1="33.255"
+          y1="-5.16669"
+          x2="38"
+          y2="74.3647"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="var(--iconColor)" />
+          <stop
+            offset="1"
+            stop-color="var(--iconColor)"
+            stop-opacity="0"
           />
-        </clipPath>
+        </linearGradient>
       </defs>
     </svg>
-  </HexagonButtonBase>
+  </button>
 </template>
