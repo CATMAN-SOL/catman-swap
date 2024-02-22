0bbc96e86e1834936494082a84408803a4da8965
Artem Tarasenko <shabashab.04@gmail.com>
2024-02-22 13:05:52 +0200

    fix(client): invalid "selected" text in dca&swap selection

--- apps/client/src/components/Choice/Dca.vue
+++ apps/client/src/components/Choice/Dca.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import StateIndicatorBottom from '../../assets/state-indicator-bottom.svg'
 const props = defineProps<{
   active: boolean
 }>()
@@ -19,20 +18,25 @@ const choose = () => {
   >
     <div
       :class="[active ? 'bg-[#21262C]' : 'bg-[#090A0B] group-hover:bg-[#21262C]']"
-      class="flex size-full items-start rounded-l-3xl px-6 py-12"
+      class="flex size-full items-start rounded-l-3xl px-6 py-12 transition-all"
     >
       <span class="text-[48px] font-semibold text-[#E2E4E9]">DCA</span>
     </div>
-    <img
-      :src="StateIndicatorBottom"
-      :class="[props.active ? 'bottom-6' : 'bottom-14']"
-      class="absolute inset-x-0 -z-10 mx-auto transition-all"
-      alt=""
-    >
-    <div>
+    <div class="absolute left-1/2 top-0 z-[-10] h-full w-[176px]  -translate-x-1/2 overflow-hidden">
+      <div
+        class="bg-primary absolute left-0 flex size-full flex-col items-center justify-end py-1 transition-all"
+        :class="{
+          'bottom-[25px]': props.active,
+          'bottom-[70px]': !props.active,
+        }"
+      >
+        <span class="font-secondary translate-x-[4px] text-[16px] font-semibold text-black">Selected</span>
+      </div>
+    </div>
+    <div class="relative h-full min-w-[168px] overflow-y-hidden">
       <svg
-        class="rotate-180"
-        width="168"
+        class="absolute bottom-0 left-[-4px] rotate-180"
+        width="176"
         height="214"
         viewBox="0 0 168 214"
         fill="none"
@@ -40,16 +44,17 @@ const choose = () => {
       >
         <path
           :class="[active ? 'fill-[#21262C]' : 'fill-[#090A0B] group-hover:fill-[#21262C]']"
+          class="transition-all"
           fill-rule="evenodd"
           clip-rule="evenodd"
-          d="M422.773 187.696C422.773 201.925 411.238 213.46 397.009 213.46H-244.581C-258.811 213.46 -270.346 201.925 -270.346 187.696V25.9092C-270.346 11.6799 -258.811 0.144867 -244.581 0.144867H-4.69836C4.63705 0.144867 13.2422 5.19475 17.7947 13.3449L33.9742 42.31C38.5268 50.4601 47.1319 55.51 56.4673 55.51H111.525C120.86 55.51 129.465 50.4601 134.018 42.31L150.197 13.3449C154.75 5.19475 163.355 0.144867 172.69 0.144867H397.009C411.238 0.144867 422.773 11.68 422.773 25.9092V187.696Z"
+          d="m 408.42399,187.696 c 0,14.229 -11.0574,26.304 -24.69726,26.304 h -615.0255 c -13.64082,0 -24.69822,-12.075 -24.69822,-26.304 V 25.9092 c 0,-14.2293 11.0574,-25.764333 24.69822,-25.764333 H -1.348272 c 8.9488852,0 17.197746,5.049883 21.561754,13.200033 L 35.723083,42.31 c 4.364103,8.1501 12.612916,13.2 21.561792,13.2 h 52.778085 c 8.94849,0 17.19721,-5.0499 21.56169,-13.2 l 15.50912,-28.9651 c 4.36449,-8.15015 12.61321,-13.200033 21.5617,-13.200033 h 215.03126 c 13.63986,0 24.69726,11.535133 24.69726,25.764333 z"
           fill="#21262C"
         />
       </svg>
     </div>
     <div
       :class="[active ? 'bg-[#21262C]' : 'bg-[#090A0B] group-hover:bg-[#21262C]']"
-      class="flex size-full items-start rounded-r-3xl px-[18px] py-12"
+      class="flex size-full items-start rounded-r-3xl px-[18px] py-12 transition-all"
     >
       <span
         :class="[props.active ? 'text-[#E2E4E9]' : 'text-[#A3A5B6]']"
